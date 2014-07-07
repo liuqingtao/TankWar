45611f88bcb870d2588c8948cbcce53622a68f69
liuqingtao <liuqingtao86@gmail.com>
2014-07-07 10:46:22 +0800

    TankWar 1.0 显示一个窗口

---
+++ src/TankWar.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+public class TankWar extends Frame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new TankWar().aLunch();
+	}
+	
+	public void aLunch(){
+		setLocation(200,200);
+		setSize(800,600);
+		setVisible(true);
+	}
+
+}
