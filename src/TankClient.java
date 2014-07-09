50b151f8520def4e40accd22ec3722b3d327e0b9
liuqingtao <liuqingtao86@gmail.com>
2014-07-09 16:31:49 +0800

    0.5 Code refactoring

--- src/TankClient.java
+++ src/TankClient.java
@@ -2,8 +2,8 @@ import java.awt.*;
 import java.awt.event.*;
 public class TankClient extends Frame{
 	
-	
-
+	public static final int GAME_WIDTH = 800;
+	public static final int GAME_HEIGHT =600;
 	public int x=50,y=50;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,7 +12,7 @@ public class TankClient extends Frame{
 	
 	public void aLunch(){
 		setLocation(200,200);
-		setSize(800,600);
+		setSize(GAME_WIDTH,GAME_HEIGHT);
 		setTitle("TankWar");
 		this.setBackground(Color.GREEN); //设置窗口的背景色
 		//添加关闭动作
@@ -38,14 +38,15 @@ public class TankClient extends Frame{
 		g.setColor(c);
 	}
 	
+//double buffer
 	public void update(Graphics g) {
 		if(offScreenImage == null){
-			offScreenImage = this.createImage(800, 600);
+			offScreenImage = this.createImage(GAME_WIDTH,GAME_HEIGHT);
 		}
 		Graphics offScreen = offScreenImage.getGraphics();
 		Color c = offScreen.getColor();
 		offScreen.setColor(Color.GREEN);
-		offScreen.fillRect(0, 0, 800, 600);
+		offScreen.fillRect(0, 0, GAME_WIDTH,GAME_HEIGHT);
 		offScreen.setColor(c);
 		paint(offScreen);
 		g.drawImage(offScreenImage, 0, 0, null);
