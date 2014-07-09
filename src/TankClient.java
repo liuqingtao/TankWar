d3b01926eabc38b3caa9f7209aa906c15794bd63
liuqingtao <liuqingtao86@gmail.com>
2014-07-09 11:28:43 +0800

    3.0 draw the tank

--- src/TankClient.java
+++ src/TankClient.java
@@ -2,6 +2,14 @@ import java.awt.*;
 import java.awt.event.*;
 public class TankClient extends Frame{
 
+	@Override
+	public void paint(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(50, 50, 30, 30); //画出坦克
+		g.setColor(c);
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new TankClient().aLunch();
@@ -11,9 +19,10 @@ public class TankClient extends Frame{
 		setLocation(200,200);
 		setSize(800,600);
 		setTitle("TankWar");
+		this.setBackground(Color.GRAY); //设置窗口的背景色
 		//添加关闭动作
 		this.addWindowListener(new WindowAdapter() {			
-			public void windowClosing(WindowEvent arg0) {
+			public void windowClosing(WindowEvent e) {
 				System.exit(0);	
 			}			
 		}
